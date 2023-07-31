dc7b7e665c777b1147ec83e6a95503fba24535fd
Glauber Torres <gthierrytorres@gmail.com>
2023-07-31 16:04:30 -0300

    commit inicial

---
+++ básicos/1.1.1_frase_na_tela.c
@@ -0,0 +1,9 @@
+/*1.1.1 Frase na tela - Implemente um programa que escreve na tela a frase "O primeiro programa a gente nunca esquece!".*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("O primeiro programa a gente nunca esquece\n");
+}
+
